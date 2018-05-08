a219317c5540d6c48db9d0092c3147bde60a91f8
Ernesto <egeregogbu@gmail.com>
2018-05-08 14:48:04 -0500

    Se corriegieron las cosas

--- prueba.c
+++ prueba.c
@@ -11,13 +11,15 @@ typedef struct
 int main() 
 { 
   coord p1, p2; 
+
   //Preguntar las coordenadas del punto 1. 
   scanf("%f", &p1.x); 
-  scanf("%f", &p1.yt); 
+  scanf("%f", &p1.y); 
 
   //Preguntar las coordenadas de l punt 2: 
-  cscanf("%g", p2.x); 
-  scanf("%f", p2.y); 
+  scanf("%f", &p2.x); 
+  scanf("%f", &p2.y); 
 
-  printf("x+x, y-y"); 
+  printf(); 
+  return 0; 
 }
