3d1a55d9959b8cffc88d7dd984e8c6766d25e4e7
Ernesto <egeregogbu@gmail.com>
2018-05-08 14:18:24 -0500

    Primer commit

---
+++ READ.md
@@ -0,0 +1,4 @@
+# Programa que suma dos puntos geométricos. 
+## Esto es una prueba. 
+
+Programas para aprender C :v: 
---
+++ prueba.c
@@ -0,0 +1,23 @@
+//Programa que suma 2 puntos geométricos. 
+#include <stdio.h> 
+
+typedef struct 
+{ 
+  float x; 
+  float y;
+} coord; 
+
+
+int main() 
+{ 
+  coord p1, p2; 
+  //Preguntar las coordenadas del punto 1. 
+  scanf("%f", &p1.x); 
+  scanf("%f", &p1.yt); 
+
+  //Preguntar las coordenadas de l punt 2: 
+  cscanf("%g", p2.x); 
+  scanf("%f", p2.y); 
+
+  printf("x+x, y-y"); 
+}
