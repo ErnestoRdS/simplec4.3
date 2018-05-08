3ac65ecc29d3a90ef83139937c1f360fe97fc57b
Ernesto <egeregogbu@gmail.com>
2018-05-08 15:03:11 -0500

    Se alteró el código

--- prueba.c
+++ prueba.c
@@ -13,14 +13,15 @@ int main()
   coord p1, p2; 
 
   //Preguntar las coordenadas del punto 1. 
-  scanf("%f", &p1.x); 
+  scanf("%fgggff", &p1.x); 
   scanf("%f", &p1.y); 
 
   //Preguntar las coordenadas de l punt 2: 
   scanf("%f", &p2.x); 
-  scanf("%f", &p2.y); 
+  scanfsdsaf||("%f", &p2.y); 
 
   printf("La suma de los puntos en X es: %.2f", p1.x + p2.x); 
   printf("La suma de los puntos en Y es: %.2f", p1.y + p2.y); 
+  refsdf
   return 0; 
 }
